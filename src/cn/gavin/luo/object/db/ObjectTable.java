a0f78bab5f6d1b6bdfdb5ca6a8cae9bfe9cdcace
Gavin Luo <Gavin.Luo@software.dell.com>
2017-06-01 11:11:40 +0800

    Function complete

--- src/cn/gavin/luo/object/db/ObjectTable.java
+++ src/cn/gavin/luo/object/db/ObjectTable.java
@@ -23,14 +23,14 @@ public class ObjectTable<T extends Serializable> {
     private HashMap<String, SoftReference<T>> cache;
 
     public ObjectTable(Class<T> table, File root) {
-        this.root = new File(root,table.getSimpleName());
+        this.root = new File(root, table.getName());
         this.table = table;
-        cache = new HashMap<String, SoftReference<T>>();
+        cache = new HashMap<>();
     }
 
     public synchronized String save(T object, String id) throws IOException {
         File entry = buildFile(id);
-        if(entry.exists()){
+        if (entry.exists()) {
             throw new IOException("Object with id: " + id + " already existed!");
         }
         saveObject(object, entry);
@@ -38,13 +38,77 @@ public class ObjectTable<T extends Serializable> {
         return id;
     }
 
-    public synchronized String update(T object, String id){
+    public synchronized String update(T object, String id) {
         File file = buildFile(id);
-        if(file.exists()){
+        if (file.exists()) {
             file.delete();
         }
         saveObject(object, file);
         return id;
+    }
+
+    public String save(T object) throws IOException {
+        if (object instanceof IDObject) {
+            if (((IDObject) object).getId() == null) {
+                ((IDObject) object).setId(UUID.randomUUID().toString());
+            }
+            return save(object, ((IDObject) object).getId());
+        } else {
+            return save(object, UUID.randomUUID().toString());
+        }
+    }
+
+    public synchronized T loadObject(String id) {
+        T object = null;
+        SoftReference<T> ref = cache.get(id);
+        if (ref != null) {
+            object = ref.get();
+        }
+        if (object == null) {
+            String name = getName(id);
+            object = load(name);
+        }
+        return object;
+    }
+
+    public List<T> loadAll() {
+        List<T> list = new ArrayList<>();
+        if (root.isDirectory()) {
+            for (File file : root.listFiles()) {
+                T entry = loadEntry(file);
+                if (entry != null)
+                    list.add(entry);
+            }
+        }
+        return list;
+    }
+
+    public synchronized void clear() {
+        if(root.isDirectory()){
+            for(File file : root.listFiles()){
+                file.delete();
+            }
+        }
+        root.delete();
+        cache.clear();
+    }
+
+    public void delete(String id) {
+        buildFile(id).delete();
+        cache.remove(id);
+    }
+
+    public void fuse() throws IOException {
+        for (SoftReference<T> ref : cache.values()) {
+            if(ref!=null) {
+                T t = ref.get();
+                if (t != null) {
+                    if (t instanceof IDObject) {
+                        save(t, ((IDObject) t).getId());
+                    }
+                }
+            }
+        }
     }
 
     private void saveObject(T object, File entry) {
@@ -58,63 +122,17 @@ public class ObjectTable<T extends Serializable> {
         }
     }
 
-    public String save(T object) throws IOException {
-        if(object instanceof IDObject){
-            if(((IDObject) object).getId() == null){
-                ((IDObject) object).setId(UUID.randomUUID().toString());
-            }
-            return save(object, ((IDObject) object).getId());
-        }else {
-            return save(object, UUID.randomUUID().toString());
-        }
-    }
-
-    public synchronized T loadObject(String id) {
-        T object = null;
-        SoftReference<T> ref = cache.get(id);
-        if(ref!=null){
-            object = ref.get();
-        }
-        if(object == null) {
-            String name = getName(id);
-            object = load(name);
-        }
-        return object;
-    }
-
-    public List<T> loadAll(){
-        List<T> list = new ArrayList<>();
-
-        return list;
-    }
-
-    public synchronized void clear() {
-
-
-    }
-
-    public void delete(String id) {
-        buildFile(id).delete();
-        cache.remove(id);
-    }
-
-    public void fuse() throws IOException {
-        for(SoftReference<T> ref : cache.values()){
-            if(ref!=null && ref.get()!=null){
-                if(ref.get() instanceof IDObject){
-                    save(ref.get(),((IDObject) ref.get()).getId());
-                }
-            }
-        }
-    }
-
     private String getName(String id) {
         return table.getName() + "@" + id;
     }
 
     private T load(String id) {
         File entry = buildFile(id);
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(entry))){
+        return loadEntry(entry);
+    }
+
+    private T loadEntry(File entry) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(entry))) {
             Object o = ois.readObject();
             ois.close();
             return table.cast(o);
@@ -124,8 +142,11 @@ public class ObjectTable<T extends Serializable> {
         return null;
     }
 
-    private File buildFile(String id){
-        return new File(root,id);
+    private File buildFile(String id) {
+        return new File(root, id);
     }
 
+    public int size(){
+        return root.isDirectory() ? root.list().length : 0;
+    }
 }
